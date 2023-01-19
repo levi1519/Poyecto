2b03342688fda7c972af3a63b8e131425850caf1
Ruben Andrade <randradec@unemi.edu.ec>
2023-01-19 12:53:00 -0500

    ver partes del codigo que estan en stage

--- happydog.py
+++ happydog.py
@@ -2,10 +2,11 @@ f = "feli"
 t = "tiste"
 
 dog = f
+dog2 = t 
 
 if dog == f:
     print("perrito feli c:")
-elif dog == t:
-    print("perrito tiste :c ")
-        
+elif dog2 == t:
+    print("perrito tiste : ")
+
 
